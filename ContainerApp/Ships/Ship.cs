5c1c60e36f476476a187b7f03e06c5bf2f4f03fd
Artur Nikytenko <arturnikytenko@gmail.com>
2024-03-21 01:41:25 +0100

    Ship init

---
+++ ContainerApp/Ships/Ship.cs
@@ -0,0 +1,21 @@
+﻿using ContainerApp.Containers;
+
+namespace ContainerApp.Ships;
+
+public class Ship
+{
+    private List<Container> _containers;
+    //knots
+    private double maximumSpeed;
+    private int maximumContainersCapacity;
+    //tons
+    private double maximumSumOfContainersWeight;
+
+    public Ship(List<Container> containers, double maximumSpeed, int maximumContainersCapacity, double maximumSumOfContainersWeight)
+    {
+        _containers = containers;
+        this.maximumSpeed = maximumSpeed;
+        this.maximumContainersCapacity = maximumContainersCapacity;
+        this.maximumSumOfContainersWeight = maximumSumOfContainersWeight;
+    }
+}
